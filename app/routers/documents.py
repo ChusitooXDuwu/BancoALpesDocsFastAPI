6e62e199414c8e7ec908a216a2c49000fec45436
chusitooxduwu <w.chu@uniandes.edu.co>
2024-05-30 03:00:29 -0500

    comit

--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,10 +1,25 @@
-from fastapi import APIRouter, HTTPException, Depends
-from ..crud import get_document_by_id, get_all_documents, create_new_document, delete_all_documents, delete_document_by_id
+from fastapi import APIRouter, HTTPException
+from ..crud import create_new_document, get_all_documents, get_document_by_id, delete_document_by_id, delete_all_documents
 from ..schemas import Documento, DocumentoCreate
-from typing import List
 import requests
 
 router = APIRouter()
+
+CLIENTE_API_URL = "http://34.170.157.181:8080/cliente/"
+
+@router.post("/documentos/", response_model=Documento)
+async def create_documento(documento: DocumentoCreate):
+    try:
+        response = requests.get(f"{CLIENTE_API_URL}{documento.cliente_id}/")
+        if response.status_code != 200:
+            raise HTTPException(status_code=404, detail="Cliente not found")
+        cliente_data = response.json()
+        if not cliente_data:
+            raise HTTPException(status_code=404, detail="Cliente not found")
+        return await create_new_document(documento)
+    except Exception as e:
+        print(f"Error creating document: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/documentos/", response_model=List[Documento])
 async def read_all_documents(skip: int = 0, limit: int = 10):
@@ -17,22 +32,6 @@ async def read_document_by_id(documento_id: str):
     if document is None:
         raise HTTPException(status_code=404, detail="Documento not found")
     return document
-
-@router.post("/documentos/", response_model=Documento)
-async def create_new_document_endpoint(documento: DocumentoCreate):
-    # try:
-    #     return await create_new_document(documento)
-    # except Exception as e:
-    #     print(f"Error creating document: {e}")
-    #     raise HTTPException(status_code=500, detail=str(e))
-    
-    try:
-        response = requests.get(f"{CLIENTE_API_URL}{documento.cliente_id}/")
-        if response.status_code != 200:
-            raise HTTPException(status_code=404, detail="Cliente not found")
-        return await create_new_document(documento)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/documentos/{documento_id}", response_model=dict)
 async def delete_document_by_id_endpoint(documento_id: str):
