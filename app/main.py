073edb1ccbe8b4575a0aaed778f053083946150f
Wyo Hann Chu <w.chu@uniandes.edu.co>
2024-05-30 09:31:06 -0500

    add middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from .routers import documents
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cambia esto según sea necesario
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(documents.router, prefix="/api")
